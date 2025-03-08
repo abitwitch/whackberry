17f47ce2569591b3e892a27c238839bbd84816c6
abitwitch <arrakis256@gmail.com>
2025-03-08 10:52:09 -0500

    playing with sender and reciever

--- receiver.py
+++ receiver.py
@@ -7,13 +7,59 @@ import time
 print("start.")
 pulses = pulseio.PulseIn(board.GP15, maxlen=200, idle_state=True)
 
+pulse_min=1000
+pulse_increment=500
+pulse_checksum=0
+bytedata = bytearray()
 
+'''
+def decodePulses(receivedPulses):
+    
+
+    
+    s = ""
+    nulled=False
+    for pulse in receivedPulses:
+        byte=(pulse-pulse_min)/pulse_increment
+        byte=round(byte)
+        if byte>255:
+            raise ValueError("Error: unknown pulse length")
+        if nulled:
+            pass
+            #if pulse_checksum!=byte:
+            #    raise ValueError("Error: bad checksum")
+        else:
+            pulse_checksum=(pulse_checksum+byte)%256
+            s+=chr(byte)
+        if byte=='\0':
+            nulled=True
+    print(s)
+    return s
+'''    
+def decodeBytes(bytedata):
+    sendKeystrokes(bytedata.decode("utf-8"))
+
+def sendKeystrokes(string):
+    print(string)
+    #TODO
 
 while True:
     s=[]
     while len(pulses)>0:
-        s.append(pulses.popleft())
-    print(s)
+        pulse=pulses.popleft()
+        byte=(pulse-pulse_min)/pulse_increment
+        byte=round(byte)
+        if byte>255:
+            byte-=2*((255*pulse_increment)+pulse_min) #minus 2xfull bytes
+            if pulse_checksum!=byte:
+                raise ValueError("Error: bad checksum")
+            else:
+                decodeBytes(bytedata)
+                pulse_checksum=0
+                bytedata = bytearray()
+        else:
+            pulse_checksum=(pulse_checksum+byte)%256
+            bytedata.append(byte)
     time.sleep(1)
     
 print("done.")
--- sender.py
+++ sender.py
@@ -32,25 +32,35 @@ kbd = Keyboard(usb_hid.devices)
 '''
 
 # 50% duty cycle at 38kHz.
-pulse = pulseio.PulseOut(board.LED, frequency=38000, duty_cycle=32768)
-#                             on   off     on    off    on
-pulses = array.array('H', [65000, 1000, 65000, 65000, 1000])
-
-
+pulseLED = pulseio.PulseOut(board.LED, frequency=38000, duty_cycle=32768)
 # 50% duty cycle at 38kHz.
-pulse2 = pulseio.PulseOut(board.GP15, frequency=38000, duty_cycle=32768)
-#                             on   off     on    off    on
-pulses2 = array.array('H', [65000, 1000, 65000, 65000, 1000])
+pulseIR = pulseio.PulseOut(board.GP14, frequency=38000, duty_cycle=32768)
 
+text="hello world"
 
+pulse_min=1000
+pulse_increment=500
+
+pulse_checksum=0
+pulses = array.array('H')
+bytedata = bytearray()
+bytedata.extend(text)
+for byte in bytedata:
+    pulse_checksum=(pulse_checksum+byte)%256
+    byte=(byte*pulse_increment)+pulse_min
+    pulses.append(byte)
+finalPulse=(255*pulse_increment)+pulse_min #max byte
+finalPulse*=2 #double
+finalPulse+=pulse_checksum #with checksum
+pulses.append(finalPulse)    
     
     
 while True:
-    pulse.send(pulses)
+    pulseLED.send(pulses)
 
     time.sleep(1)
     
-    pulse2.send(pulses2)
+    pulseIR.send(pulses)
 
     
 
