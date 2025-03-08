3312939e3336d04bb54753e2bdcbbcb0adb3c03a
abitwitch <arrakis256@gmail.com>
2025-03-08 12:15:47 -0500

    send and receive working

--- receiver.py
+++ receiver.py
@@ -3,64 +3,48 @@ import board
 import pulseio
 import time
 
-
 print("start.")
 pulses = pulseio.PulseIn(board.GP15, maxlen=200, idle_state=True)
 
 pulse_min=1000
-pulse_increment=500
+pulse_increment=250
 pulse_checksum=0
 bytedata = bytearray()
+status="ready"
 
-'''
-def decodePulses(receivedPulses):
-    
-
-    
-    s = ""
-    nulled=False
-    for pulse in receivedPulses:
-        byte=(pulse-pulse_min)/pulse_increment
-        byte=round(byte)
-        if byte>255:
-            raise ValueError("Error: unknown pulse length")
-        if nulled:
-            pass
-            #if pulse_checksum!=byte:
-            #    raise ValueError("Error: bad checksum")
-        else:
-            pulse_checksum=(pulse_checksum+byte)%256
-            s+=chr(byte)
-        if byte=='\0':
-            nulled=True
-    print(s)
-    return s
-'''    
-def decodeBytes(bytedata):
-    sendKeystrokes(bytedata.decode("utf-8"))
+def bytearray_to_text(bytedata):
+    #todo add crypt
+    return(bytedata.decode("utf-8"))
 
 def sendKeystrokes(string):
     print(string)
     #TODO
 
 while True:
-    s=[]
     while len(pulses)>0:
         pulse=pulses.popleft()
         byte=(pulse-pulse_min)/pulse_increment
         byte=round(byte)
-        if byte>255:
-            byte-=2*((255*pulse_increment)+pulse_min) #minus 2xfull bytes
-            if pulse_checksum!=byte:
-                raise ValueError("Error: bad checksum")
-            else:
-                decodeBytes(bytedata)
-                pulse_checksum=0
-                bytedata = bytearray()
-        else:
+        if byte==256:
+            status="datastream"
+            pulse_checksum=0
+            bytedata=bytearray()
+        elif byte>256:
+            status="checksum"
+        elif byte<0:
+            status="error"
+            print("Warning: unexpected pulse length")
+        elif status=="datastream":
             pulse_checksum=(pulse_checksum+byte)%256
             bytedata.append(byte)
-    time.sleep(1)
+        elif status=="checksum":
+            if pulse_checksum!=byte:
+                print("Error: bad checksum")
+            else:
+                text=bytearray_to_text(bytedata)
+                sendKeystrokes(text)
+            status="ready"
+    #time.sleep(1)
     
 print("done.")
 
--- sender.py
+++ sender.py
@@ -1,70 +1,55 @@
 #Pico IR reciever
-import time
 import board
-import digitalio
 import time
-import board
-
 import array
 import pulseio
-
-'''
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
-
-
-for x in range(2):
-    led.value = True
-    time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
-
-import usb_hid
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keycode import Keycode
-
-
-
-print("start.")
-
-# Set up a keyboard device.
-kbd = Keyboard(usb_hid.devices)
-'''
 
 # 50% duty cycle at 38kHz.
 pulseLED = pulseio.PulseOut(board.LED, frequency=38000, duty_cycle=32768)
 # 50% duty cycle at 38kHz.
 pulseIR = pulseio.PulseOut(board.GP14, frequency=38000, duty_cycle=32768)
 
-text="hello world"
-
+#Allows for unique 258 pulses
+#0-255: data
+#256: start data stream
+#257: end stream
+#258: unused
 pulse_min=1000
-pulse_increment=500
+pulse_increment=250
 
-pulse_checksum=0
-pulses = array.array('H')
-bytedata = bytearray()
-bytedata.extend(text)
-for byte in bytedata:
-    pulse_checksum=(pulse_checksum+byte)%256
-    byte=(byte*pulse_increment)+pulse_min
-    pulses.append(byte)
-finalPulse=(255*pulse_increment)+pulse_min #max byte
-finalPulse*=2 #double
-finalPulse+=pulse_checksum #with checksum
-pulses.append(finalPulse)    
-    
-    
+def text_to_bytearray(text):
+    #todo add crypt
+    bytedata = bytearray()
+    bytedata.extend(text)
+    return(bytedata)
+
+def bytearray_to_pulsearray(bytearray: bytedata):    
+    pulse_checksum=0
+    pulses = array.array('H')
+    startStreamPulse=(256*pulse_increment)+pulse_min #max byte+1
+    pulses.append(startStreamPulse)
+    for byte in bytedata:
+        pulse_checksum=(pulse_checksum+byte)%256
+        byte=(byte*pulse_increment)+pulse_min
+        pulses.append(byte)
+    endStreamPulse=(257*pulse_increment)+pulse_min #max byte+2
+    checksumPulse=(pulse_checksum*pulse_increment)+pulse_min
+    pulses.append(endStreamPulse)
+    pulses.append(checksumPulse)
+    pulses.append(endStreamPulse) #extra pulse seems to be needed for a clean array to be received by reveiver
+    return(pulses)
+
+text="hello world"
+bytedata=text_to_bytearray(text)
+pulses=bytearray_to_pulsearray(bytedata)
+
 while True:
+    time.sleep(0.5)
+    
     pulseLED.send(pulses)
 
-    time.sleep(1)
+    time.sleep(0.5)
     
     pulseIR.send(pulses)
-
-    
-
-print("done.")
-
 
 
