8fdeb4d8bd71cadc3ecc55e0de5fc203cd7c4252
abitwitch <arrakis256@gmail.com>
2025-03-30 12:15:23 -0400

    Updates to receiver.py from rpi

--- receiver.py
+++ receiver.py
@@ -4,47 +4,91 @@ import pulseio
 import time
 
 print("start.")
-pulses = pulseio.PulseIn(board.GP15, maxlen=200, idle_state=True)
+pulses = pulseio.PulseIn(board.GP3, maxlen=200, idle_state=True)
 
+#Allows for n unique pulse lengths
+#0: start data stream
+#1: end stream
+#2: start instructions
+#3 to n: data
+pulses_per_byte=2
+if not pulses_per_byte in [1,2,4,8]:
+  raise ValueError(f"Error: can not split a byte into {pulses_per_byte} parts.")
+bits_per_pulse=int(8/pulses_per_byte)
+n=int(256/pulses_per_byte)+3
 pulse_min=1000
-pulse_increment=250
-pulse_checksum=0
+pulse_max=(2**16)-pulse_min
+#special pulses
+start_val=0
+end_val=1
+inst_val=2
+minData_val=3
+pulse_increment=(pulse_max-pulse_min)/(n+3)
+#pulses per packet
+packet_size=10 #limitation of ir-ctl
+
+checksum=checksum_ver=0
+byte_part=byte=0
 bytedata = bytearray()
-status="ready"
+stream_status="ready"
+packet_status="ready"
+
 
 def bytearray_to_text(bytedata):
-    #todo add crypt
-    return(bytedata.decode("utf-8"))
+  #todo add crypt
+  return(bytedata.decode("utf-8"))
 
 def sendKeystrokes(string):
-    print(string)
-    #TODO
-
+  print(string)
+  #TODO
+      
 while True:
-    while len(pulses)>0:
-        pulse=pulses.popleft()
-        byte=(pulse-pulse_min)/pulse_increment
-        byte=round(byte)
-        if byte==256:
-            status="datastream"
-            pulse_checksum=0
-            bytedata=bytearray()
-        elif byte>256:
-            status="checksum"
-        elif byte<0:
-            status="error"
-            print("Warning: unexpected pulse length")
-        elif status=="datastream":
-            pulse_checksum=(pulse_checksum+byte)%256
-            bytedata.append(byte)
-        elif status=="checksum":
-            if pulse_checksum!=byte:
-                print("Error: bad checksum")
-            else:
-                text=bytearray_to_text(bytedata)
-                sendKeystrokes(text)
-            status="ready"
-    #time.sleep(1)
-    
+  while len(pulses)>0:
+    pulse=pulses.popleft()
+    pulse_val=(pulse-pulse_min)/pulse_increment
+    pulse_val=round(pulse_val)
+    if pulse_val==start_val and stream_status=="ready" and packet_status=="receiving":
+      stream_status="datastream"
+    elif pulse_val==inst_val:
+      if packet_status=="ready":
+        stream_status="instruction"
+      if packet_status=="receiving":
+        stream_status="checksum"
+    elif pulse_val==end_val:
+      if stream_status=="instruction":
+        #prepare new stream
+        packet_status="receiving"
+        stream_status="ready"
+        checksum=checksum_ver=0
+        bytedata = bytearray()
+      elif stream_status=="checksum":
+        if checksum==checksum_ver:
+          bytearray_to_text(bytedata)
+        else:
+          print("Error: bad checksum, exiting data stream.")
+        checksum=checksum_ver=0
+        bytedata = bytearray()
+        packet_status="ready"
+        stream_status="ready"
+      else:
+        stream_status="ready"
+    elif 0 > pulse_val > n:
+      print("Warning: unexpected pulse length, ignoring pulse.")
+    elif stream_status in ["datastream","checksum"] and packet_status=="receiving":
+      pulse_val-=3  #for the 3 special values
+      byte+=pulse_val<<(bits_per_pulse*byte_part)
+      byte_part+=1
+      if byte_part==pulses_per_byte:
+        if stream_status=="checksum":
+          checksum_ver=byte
+        if stream_status=="datastream":
+          bytedata.append(byte)
+          checksum=(checksum+byte)%256
+        byte_part=byte=0
+    else:
+      pass #this is where any unexpected values or padding values used to buffer packets will end up. They can be ignored.
+
+  time.sleep(1)
+  
 print("done.")
 
