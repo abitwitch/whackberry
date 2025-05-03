5ae2cc67de239797dd4afafac15ba9314391bab5
abitwitch <arrakis256@gmail.com>
2025-05-02 21:08:10 -0400

    fixing mapping mouse pins

--- navmouse/mouse.py
+++ navmouse/mouse.py
@@ -2,18 +2,22 @@ import time
 from pynput.mouse import Button, Controller
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
-leftPin=13
-rightPin=15
+speed=5 #1-10
+leftPin=15
+rightPin=23
 upPin=19
-downPin=21
-centerPin=23
+downPin=13
+centerPin=21
+rightclickPin=27
 GPIO.setup(leftPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(rightPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(upPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(downPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(centerPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(rightclickPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 centerPin_prevval=GPIO.input(centerPin)==GPIO.HIGH
+rightclick_prevval=GPIO.input(rightclick)==GPIO.HIGH
 mouseReady=False
 while True:
   if not mouseReady:
@@ -28,15 +32,20 @@ while True:
   if GPIO.input(rightPin)==GPIO.HIGH:
     mouse.move(1,0)
   if GPIO.input(upPin)==GPIO.HIGH:
-    mouse.move(0,1)
-  if GPIO.input(downPin)==GPIO.HIGH:
     mouse.move(0,-1)
+  if GPIO.input(downPin)==GPIO.HIGH:
+    mouse.move(0,1)
   centerPin_curval=GPIO.input(centerPin)==GPIO.HIGH
   if centerPin_curval!=centerPin_prevval:
     centerPin_prevval=centerPin_curval
     if centerPin_curval:
       mouse.click(Button.left)
-  time.sleep(0.01)
+  rightclickPin_curval=GPIO.input(rightclickPin)==GPIO.HIGH
+  if rightclickPin_curval!=rightclick_prevval:
+    rightclick_prevval=rightclickPin_curval
+    if rightclick_prevval:
+      mouse.click(Button.right)
+  time.sleep(0.001/speed)
  
  
 
